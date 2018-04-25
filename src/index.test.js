ffc88df6ea58bd4067782f42aeb8bf5b522b2507
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2018-04-24 21:06:31 -0400

    test(index.test.js): added test script
    re #3

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var rdn = require('./index');
+
+describe('demo-names', function() {
+    it('should work', function() {
+        expect(true).to.be.true;
+    });
+});
