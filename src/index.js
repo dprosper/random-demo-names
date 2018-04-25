cfba28e5825a1c87e7ea651f5283a1a36ccf3d82
Dimitri Prosper <dimitri_prosper@us.ibm.com>
2018-04-24 20:38:26 -0400

    feat(index.js): added the ability to specify how many random names are returned
    this is a very long description of the same thing over and over again but just for fun needed to add
    something very long in here to see what it looks like in the results
    
    re #2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 var uniqueRandomArray = require("unique-random-array");
 var names = require("./demo-names");
-
+var getRandomItem = uniqueRandomArray(names)
 module.exports = {
   all: names,
-  random: uniqueRandomArray(names)
+  random: random
 };
+
+function random (number) {
+  if(number === undefined) {
+    return getRandomItem();
+  } else {
+    var randomItems = [];
+    for (var i=0; i<number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
